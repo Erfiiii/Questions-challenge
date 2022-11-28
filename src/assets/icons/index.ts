6c9767e960df283267ac6749b739a73a5539e298
Erfan Hasanzadeh <er.hasanzadeh@gmail.com>
2022-11-29 01:59:01 +0330

    add svg icons to assets

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,6 @@
+export { ReactComponent as SadIcon } from './SadIcon.svg';
+export { ReactComponent as MessageIcon } from './MessageIcon.svg';
+export { ReactComponent as SmileIcon } from './SmileIcon.svg';
+export { ReactComponent as AvatarIcon } from './AvatarIcon.svg';
+export { ReactComponent as PlusIcon } from './PlusIcon.svg';
+export { ReactComponent as CloseIcon } from './CloseIcon.svg';
